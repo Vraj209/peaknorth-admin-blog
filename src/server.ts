30568d1bea5a01a1a36ce61dbbd22080b333a7fd
Vraj Panchal <80612219+Vraj209@users.noreply.github.com>
2025-10-14 09:57:52 -0400

    update backend port

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,7 @@ if (featureFlagsConfig.enableCors) {
     origin: [
       urlsConfig.frontend,
       urlsConfig.adminDashboard,
-      'http://localhost:3000',
+      'http://localhost:3001',
       'http://localhost:5173',
       'http://localhost:8080',
     ],
